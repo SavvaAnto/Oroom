5c8e829da7e7015b1f644fcb6709dc892e404195
w0ckeezz <savva.antonyuk.spb@gmail.com>
2020-05-03 01:55:57 +0300

    The first commit
    React + Grid + Initial Componenting

---
+++ src/components/Content.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Content = () => {
+    return (
+        <div className='content'>
+            <div className='room'>
+                <img src='https://www.gamercamp.ca/wp-content/uploads/2019/03/5-ways-to-pimp-out-your-living-room-for-ultimate-gaming.jpg'></img>
+            </div>
+            <div className='owner'>
+                <img src='https://store.playstation.com/store/api/chihiro/00_09_000/container/RU/ru/999/EP2402-CUSA05624_00-AV00000000000107/1576760032000/image?w=240&h=240&bg_color=000000&opacity=100&_version=00_09_000'></img>
+            </div>
+            <div>
+                My posts
+          <div>
+                    New post
+          </div>
+                <div>
+                    <div>
+                        Post 1
+            </div>
+                    <div>
+                        Post 2
+            </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Content;
---
+++ src/components/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <nav className='nav'>
+            <div>
+                <a>My room</a>
+            </div>
+            <div>
+                <a>My friends</a>
+            </div>
+            <div>
+                <a>Activities</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Nav;
